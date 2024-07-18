5743e4b31d7b747de476215349083fde3a153061
Tahar NAILI DOUAOUDA <97195868+taharnaili@users.noreply.github.com>
2024-07-18 03:54:03 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,41 +2,40 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
-use Filament\Filament;
 
-// Liste des utilisateurs
-Route::get('/filament/users', function () {
-    return Filament::resources('App\Filament\Resources\UserResource');
-});
+use Filament\Http\Middleware\Authenticate;
+use App\Http\Controllers\UserController;
 
-// Afficher le formulaire de création d'utilisateur
-Route::get('/filament/users/create', function () {
-    return Filament::createResource('App\Filament\Resources\UserResource');
-});
 
-// Enregistrer un nouvel utilisateur
-Route::post('/filament/users', function () {
-    return Filament::saveResource('App\Filament\Resources\UserResource');
-});
 
-// Afficher un utilisateur spécifique
-Route::get('/filament/users/{id}', function ($id) {
-    return Filament::viewResource('App\Filament\Resources\UserResource', $id);
-});
+Route::group(['middleware' => ['web']], function () {
+    // Route pour afficher le formulaire de connexion
+    Route::get('admin/login', [\Filament\Http\Controllers\AuthController::class, 'showLoginForm'])->name('filament.auth.login');
 
-// Afficher le formulaire de modification d'un utilisateur
-Route::get('/filament/users/{id}/edit', function ($id) {
-    return Filament::editResource('App\Filament\Resources\UserResource', $id);
-});
+    // Route pour traiter la requête de connexion
+    Route::post('admin/login', [\Filament\Http\Controllers\AuthController::class, 'login'])->name('filament.auth.login.post');
 
-// Mettre à jour un utilisateur existant
-Route::put('/filament/users/{id}', function ($id) {
-    return Filament::updateResource('App\Filament\Resources\UserResource', $id);
-});
+      // Liste des utilisateurs
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
-// Supprimer un utilisateur
-Route::delete('/filament/users/{id}', function ($id) {
-    return Filament::deleteResource('App\Filament\Resources\UserResource', $id);
+    // Afficher le formulaire de création d'utilisateur
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+
+    // Enregistrer un nouvel utilisateur
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+
+    // Afficher un utilisateur spécifique
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+
+    // Afficher le formulaire de modification d'un utilisateur
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+
+    // Mettre à jour un utilisateur existant
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+
+    // Supprimer un utilisateur
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
+
 });
 
 
@@ -45,6 +44,7 @@ Route::delete('/filament/users/{id}', function ($id) {
 Route::get('/', function () {
     return view('welcome');
 });
+
 
 Route::get('admin/tasks/modal/{record}',[TaskController::class, 'editStatus'])->name('task.modal');
 
